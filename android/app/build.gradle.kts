fe82904204cb8e0ee20be4c3e9f573e88065a7f8
Nillan Sivarasa <nillan.sivarasa@gmail.com>
2026-01-23 16:04:02 +0100

    Add Notification System compatible with Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,10 @@ android {
     namespace = "com.example.PeerReal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    compileSdk = 36
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -28,6 +30,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -37,6 +40,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
